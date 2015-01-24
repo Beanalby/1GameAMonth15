97235cd45b79560de0dfb91de0d3c76dc35f3054
Jason Viers <bean@beanalby.net>
2015-01-23 22:04:42 -0500

    Initial commit

---
+++ Assets/01/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace onegam_1501 {
+    [RequireComponent(typeof(CharacterController2D))]
+    public class Player: MonoBehaviour {
+
+        private float moveSpeed = 5;
+
+        private CharacterController2D cc;
+
+        // Use this for initialization
+        void Start() {
+            cc = GetComponent<CharacterController2D>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            cc.move(new Vector3(
+                moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"),
+                moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"),
+                0));
+        }
+    }
+}
