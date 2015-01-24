d608df7facd038c21ff10b7d1fb68561eced9fe9
Jason Viers <bean@beanalby.net>
2015-01-24 11:49:00 -0500

    Added basic movement and textures

--- Assets/01/Scripts/Player.cs
+++ Assets/01/Scripts/Player.cs
@@ -5,7 +5,7 @@ namespace onegam_1501 {
     [RequireComponent(typeof(CharacterController2D))]
     public class Player: MonoBehaviour {
 
-        private float moveSpeed = 5;
+        private float moveSpeed = 10;
 
         private CharacterController2D cc;
 
@@ -16,10 +16,21 @@ namespace onegam_1501 {
 
         // Update is called once per frame
         void Update() {
-            cc.move(new Vector3(
+            HandleMovement();
+        }
+
+        void HandleMovement() {
+            Vector3 delta = new Vector3(
                 moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"),
                 moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"),
-                0));
+                0);
+             //cap delta based on our movement restrictions
+            if (delta.y > 0) {
+                delta.y = Mathf.Min(Stage.yMax - transform.position.y, delta.y);
+            } else {
+                delta.y = Mathf.Max(Stage.yMin - transform.position.y, delta.y);
+            }
+            cc.move(delta);
         }
     }
 }
---
+++ Assets/01/Scripts/Stage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace onegam_1501 {
+    public class Stage: MonoBehaviour {
+        public const float yMax = -.5f;
+        public const float yMin = -13;
+
+        private static Stage _instance = null;
+        public static Stage Instance {
+            get { return _instance; }
+        }
+
+        public void Awake() {
+            if (_instance != null) {
+                Debug.LogError("Can't have more than one stage");
+                Destroy(gameObject);
+            }
+            _instance = this;
+        }
+    }
+}
